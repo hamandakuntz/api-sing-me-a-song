9f0dd403d8b732081b2c509a4ff3130a7429165c
Guga <contato.gustavobarbosa@outlook.com>
2021-08-02 16:39:55 -0300

    Preparando revisão do código da Hamanda

--- src/schemas/postRecommendation.ts
+++
@@ -1,8 +0,0 @@
-import joi from 'joi';
-
-const schemaPostRecommendation = joi.object({
-  name: joi.string().min(3).required(),
-  youtubeLink: joi.string().pattern(/^(https?\:\/\/)?(www\.)?(youtube\.com|youtu\.?be)\/.+$/).required()
-});
-
-export { schemaPostRecommendation };
--- tests/utils/database.ts
+++
@@ -1,9 +0,0 @@
-import connection from "../../src/database";
-
-export async function cleanDatabase() {
-    await connection.query(`TRUNCATE recommendations RESTART IDENTITY`);
-}
-
-export function endDatabaseConnection() {
-    connection.end();
-}
